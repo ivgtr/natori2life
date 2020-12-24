d174ff6193f479c9855c09bb99a1290c34dcec60
ivgtr <gear.1112t.o@gmail.com>
2020-12-24 18:57:10 +0900

    :sparkles: add: 配信データを取得して表示するよ

--- src/background.ts
+++ src/background.ts
@@ -4,9 +4,18 @@ chrome.tabs.onActivated.addListener((_) => {
       tab[0].status === 'loading' &&
       (tab[0].pendingUrl === 'chrome://newtab/' || tab[0].url === 'chrome://newtab/')
     ) {
+      const num = Math.floor(Math.random() * 4) + 1
       const audio = new Audio()
-      audio.src = chrome.extension.getURL('/voice/new-tab.mp3')
+      audio.src = chrome.extension.getURL(`/voice/new-tab-${num}.mp3`)
       audio.play()
     }
   })
+})
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  chrome.tabs.query({ active: true, lastFocusedWindow: true }, (tabs) => {
+    const { index } = tabs[0]
+    sendResponse(index)
+  })
+  return true
 })
--- src/popup/Popup.tsx
+++ src/popup/Popup.tsx
@@ -1,11 +1,89 @@
-import React, { useEffect } from 'react'
-// import "./Popup.scss";
+import React, { useEffect, useState } from 'react'
+import './Popup.scss'
+
+declare global {
+  interface Window {
+    io: SocketIOClientStatic
+  }
+}
+
+type liveData = {
+  id: string
+  url: string
+  thumbnail: string
+  title: string
+  description: string
+  state: string
+  channelTitle: string
+  channelId: string
+}
 
 export default function Popup() {
+  const [loading, setLoading] = useState(true)
+  const [liveData, setLiveData] = useState<liveData[]>([])
+  const [tabIndex, setTabIndex] = useState(0)
   useEffect(() => {
-    // Example of how to send a message to eventPage.ts.
-    chrome.runtime.sendMessage({ popupMounted: true })
+    chrome.runtime.sendMessage({ popupMounted: true }, (index) => {
+      setTabIndex(index)
+    })
+    const data = async () => {
+      await fetch(
+        'https://script.google.com/macros/s/AKfycbxSsBb1T0Mb46CCY3SsCl_d0lNtgngiy2kDGlULqU9Cz8oVfbMj/exec',
+        {
+          method: 'GET'
+        }
+      )
+        .then((response) => response.json())
+        .then((data) => {
+          setLoading(false)
+          if (data.status === 200) {
+            setLiveData(data.data)
+          } else {
+            throw data.message
+          }
+        })
+        .catch((error) => {
+          alert(error)
+        })
+    }
+    data()
   }, [])
 
-  return <div className="popupContainer">Hello, world!</div>
+  const openLivePage = (url: string) => {
+    chrome.tabs.create({
+      index: tabIndex + 1,
+      url,
+      selected: true
+    })
+  }
+
+  return (
+    <div className="popupContainer">
+      {loading ? (
+        <p>loadなう</p>
+      ) : liveData.length > 0 ? (
+        liveData.map((data) => {
+          return (
+            <div key={data.id} className="contents">
+              <a
+                onClick={() => {
+                  openLivePage(data.url)
+                }}
+              >
+                <div className="thumbnail">
+                  <img src={data.thumbnail} alt={data.title} />
+                </div>
+                <div className="description">
+                  <h3>{data.title}</h3>
+                  <p>{data.description}</p>
+                </div>
+              </a>
+            </div>
+          )
+        })
+      ) : (
+        <p>データがないよ</p>
+      )}
+    </div>
+  )
 }
