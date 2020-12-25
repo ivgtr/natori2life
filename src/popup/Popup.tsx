4908ac821041abe998f738a3282733c9a58717d9
ivgtr <gear.1112t.o@gmail.com>
2020-12-25 13:50:55 +0900

    :sparkles: add: いろいろ変更

--- src/popup/Popup.tsx
+++ src/popup/Popup.tsx
@@ -1,5 +1,64 @@
 import React, { useEffect, useState } from 'react'
+import './reset.scss'
 import './Popup.scss'
+
+const dummy = [
+  {
+    id: 'a',
+    url: 'https://i.ytimg.com/vi/egnnzp19Sqw/hqdefault_live.jpg',
+    thumbnail: 'https://img.youtube.com/vi/swpaOrubkT0/mqdefault.jpg',
+    title: 'さなちゃんねるサムネイル選手権！～雑振り返りを添えて～',
+    description:
+      '【企画内容】 Twitterのハッシュタグ #人間以外動物家族描いて名取 にて募集した同居ご家族動物の写真を絵にしていく！ 眠くなったら終了！バブちゃんなので… （最初6 ...',
+    state: 'upcoming',
+    channelTitle: 'a',
+    channelId: 'a'
+  },
+  {
+    id: 'b',
+    url: 'https://i.ytimg.com/vi/egnnzp19Sqw/hqdefault_live.jpg',
+    thumbnail: 'https://img.youtube.com/vi/swpaOrubkT0/mqdefault.jpg',
+    title: 'さなちゃんねるサムネイル選手権！～雑振り返りを添えて～',
+    description:
+      '【企画内容】 Twitterのハッシュタグ #人間以外動物家族描いて名取 にて募集した同居ご家族動物の写真を絵にしていく！ 眠くなったら終了！バブちゃんなので… （最初6 ...',
+    state: 'none',
+    channelTitle: 'a',
+    channelId: 'a'
+  },
+  {
+    id: 'c',
+    url: 'https://i.ytimg.com/vi/egnnzp19Sqw/hqdefault_live.jpg',
+    thumbnail: 'https://img.youtube.com/vi/swpaOrubkT0/mqdefault.jpg',
+    title: 'さなちゃんねるサムネイル選手権！～雑振り返りを添えて～',
+    description:
+      '【企画内容】 Twitterのハッシュタグ #人間以外動物家族描いて名取 にて募集した同居ご家族動物の写真を絵にしていく！ 眠くなったら終了！バブちゃんなので… （最初6 ...',
+    state: 'none',
+    channelTitle: 'a',
+    channelId: 'a'
+  },
+  {
+    id: 'd',
+    url: 'https://i.ytimg.com/vi/egnnzp19Sqw/hqdefault_live.jpg',
+    thumbnail: 'https://img.youtube.com/vi/swpaOrubkT0/mqdefault.jpg',
+    title: 'さなちゃんねるサムネイル選手権！～雑振り返りを添えて～',
+    description:
+      '【企画内容】 Twitterのハッシュタグ #人間以外動物家族描いて名取 にて募集した同居ご家族動物の写真を絵にしていく！ 眠くなったら終了！バブちゃんなので… （最初6 ...',
+    state: 'none',
+    channelTitle: 'a',
+    channelId: 'a'
+  },
+  {
+    id: 'e',
+    url: 'https://i.ytimg.com/vi/egnnzp19Sqw/hqdefault_live.jpg',
+    thumbnail: 'https://img.youtube.com/vi/swpaOrubkT0/mqdefault.jpg',
+    title: 'さなちゃんねるサムネイル選手権！～雑振り返りを添えて～',
+    description:
+      '【企画内容】 Twitterのハッシュタグ #人間以外動物家族描いて名取 にて募集した同居ご家族動物の写真を絵にしていく！ 眠くなったら終了！バブちゃんなので… （最初6 ...',
+    state: 'none',
+    channelTitle: 'a',
+    channelId: 'a'
+  }
+]
 
 declare global {
   interface Window {
@@ -20,7 +79,7 @@ type liveData = {
 
 export default function Popup() {
   const [loading, setLoading] = useState(true)
-  const [liveData, setLiveData] = useState<liveData[]>([])
+  const [liveDataList, setLiveDataList] = useState<liveData[]>([])
   const [tabIndex, setTabIndex] = useState(0)
   useEffect(() => {
     chrome.runtime.sendMessage({ popupMounted: true }, (index) => {
@@ -37,7 +96,7 @@ export default function Popup() {
         .then((data) => {
           setLoading(false)
           if (data.status === 200) {
-            setLiveData(data.data)
+            setLiveDataList(data.data)
           } else {
             throw data.message
           }
@@ -47,6 +106,10 @@ export default function Popup() {
         })
     }
     data()
+    // setTimeout(() => {
+    //   setLoading(false)
+    //   setLiveDataList(dummy)
+    // }, 1000)
   }, [])
 
   const openLivePage = (url: string) => {
@@ -58,26 +121,42 @@ export default function Popup() {
   }
 
   return (
-    <div className="popupContainer">
-      <div className="popupWrapper">
+    <div className="popupWrapper">
+      <div className="popupContainer">
         {loading ? (
           <p>loadなう</p>
-        ) : liveData.length > 0 ? (
-          <ul className="list">
-            {liveData.map((data) => {
+        ) : liveDataList.length > 0 ? (
+          <ul className="popupList">
+            {liveDataList.map((liveData) => {
               return (
-                <li key={data.id} className="contents">
+                <li
+                  key={liveData.id}
+                  className={[
+                    'popupListItem',
+                    liveData.state === 'upcoming'
+                      ? 'popupUpcoming'
+                      : liveData.state === 'live'
+                      ? 'popupLive'
+                      : 'popupCompleted'
+                  ].join(' ')}
+                >
+                  {liveData.state === 'upcoming' ? (
+                    <p className="popupLabel">次の配信予定</p>
+                  ) : (
+                    liveData.state === 'live' && <p className="popupLabel">現在配信中</p>
+                  )}
                   <a
                     onClick={() => {
-                      openLivePage(data.url)
+                      openLivePage(liveData.url)
                     }}
+                    className="popupListItemCard"
                   >
-                    <div className="thumbnail">
-                      <img src={data.thumbnail} alt={data.title} />
+                    <div className="popupThumbnail">
+                      <img src={liveData.thumbnail} alt={liveData.title} />
                     </div>
-                    <div className="description">
-                      <h3>{data.title}</h3>
-                      <p>{data.description}</p>
+                    <div className="popupDescription">
+                      <h3>{liveData.title}</h3>
+                      <p>{liveData.description}</p>
                     </div>
                   </a>
                 </li>
@@ -85,9 +164,15 @@ export default function Popup() {
             })}
           </ul>
         ) : (
-          <p>データがないよ</p>
+          <div className="popupNone">
+            <p className="popupNoneDescription">データがないよ</p>
+            <img
+              className="popupNoneImage"
+              src={chrome.extension.getURL(`/image/sana-enen.png`)}
+              alt="さなえんえん"
+            />
+          </div>
         )}
-        <div className="popupFooter">footer</div>
       </div>
     </div>
   )
